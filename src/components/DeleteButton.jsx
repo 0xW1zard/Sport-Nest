fa878d85f02eabc47cb734670a0028fa202751b3
Himel Ahmed <ahimel963@gmail.com>
2026-06-09 20:13:00 +0600

    Implement delete functionality in DeleteButton component; add toast notifications for success and error handling

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,8 +1,32 @@
 'use client';
 import React from 'react';
-import {AlertDialog, Button} from "@heroui/react";
+import { AlertDialog, Button } from "@heroui/react";
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 const DeleteButton = ({ name, id }) => {
+    const router = useRouter();
+
+    const handleDelete = async (id) => {
+        console.log(id)
+        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/bookings/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const res = await response.json()
+
+        console.log(res)
+        if (res.deletedCount > 0) {
+            toast.success('Booking Deleted')
+            router.refresh()
+        } else {
+            toast.error('booking not deleted')
+        }
+
+
+    }
 
     return (
         <div>
@@ -26,7 +50,7 @@ const DeleteButton = ({ name, id }) => {
                                 <Button slot="close" variant="tertiary">
                                     Cancel
                                 </Button>
-                                <Button slot="close" variant="danger">
+                                <Button slot="close" variant="danger" onClick={() => handleDelete(id)}>
                                     Delete Booking
                                 </Button>
                             </AlertDialog.Footer>
